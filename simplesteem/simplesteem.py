01f089d020d0cb6dab888f23b786b90002039eae
ArtoLabs <artopium@gmail.com>
2018-06-18 11:05:31 -0600

    fixed bugs

--- simplesteem/simplesteem.py
+++ simplesteem/simplesteem.py
@@ -4,15 +4,12 @@ import time
 import re
 import imp
 import os
-
 from steem import Steem
 from steem.post import Post
 from steem.amount import Amount
 from steem.converter import Converter
 from steembase.account import PrivateKey
-
 from screenlogger.screenlogger import Msg
-
 from simplesteem import util
 from simplesteem import makeconfig
 from simplesteem import steemconnectutil
@@ -22,15 +19,13 @@ from simplesteem import steemconnectutil
 class SimpleSteem:  
 
 
-    def __init__(self, **kwargs):
 
+    def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
-
         try:
-            print (os.path.dirname(os.path.realpath(__file__)))
-            imp.find_module('config', [os.path.dirname(os.path.realpath(__file__))])
-
+            imp.find_module('config', 
+                [os.path.dirname(os.path.realpath(__file__))])
         except ImportError:
             makeconfig.MakeConfig().setup()
             
@@ -40,46 +35,36 @@ class SimpleSteem:
             self.mainaccount
         except:
             self.mainaccount = config.mainaccount
-
         try:
             self.keys
         except:
             self.keys = config.keys
-
         try:
             self.nodes
         except:
             self.nodes = config.nodes
-
         try:
             self.client_id
         except:
             self.client_id = config.client_id
-
         try:
             self.client_secret
         except:
             self.client_secret = config.client_secret
-
         try:
             self.callback_url
         except:
             self.callback_url = config.callback_url
-
         try:
             self.permissions
         except:
             self.permissions = config.permissions
 
         self.s = None
-
         self.c = None
-
         self.msg = Msg()
-
         self.util = util.Util()
-
-        self.sc = steemconnectutil.SteemConnect(self.client_id, 
+        self.connect = steemconnectutil.SteemConnect(self.client_id, 
             self.client_secret, self.callback_url, self.permissions)
 
 
@@ -87,12 +72,13 @@ class SimpleSteem:
     def steem_instance(self):
         if self.s:
             return self.s
-
         for x in range(3):
             try:
-                self.s = Steem(keys=self.keys, nodes=[self.util.goodnode(self.nodes)])
+                self.s = Steem(keys=self.keys, 
+                    nodes=[self.util.goodnode(self.nodes)])
             except Exception as e:
-                self.util.retry("COULD NOT GET STEEM INSTANCE", e, num_of_retries, 60)
+                self.util.retry("COULD NOT GET STEEM INSTANCE", 
+                    e, num_of_retries, 60)
             else:
                 return self.s
         return False
@@ -100,42 +86,66 @@ class SimpleSteem:
 
 
     def claim_rewards(self):
-        if self.steem_instance().claim_reward_balance(account=self.mainaccount):
+        if self.steem_instance().claim_reward_balance(
+                account=self.mainaccount):
             time.sleep(5)
             return True
         else:
             return False
 
 
-    def verify_key (self, acctname, private_posting_key):
-        pubkey = PrivateKey(private_posting_key).pubkey or 0
-        pubkey2 = self.steem_instance().get_account(acctname)['posting']['key_auths'][0][0]
-        if str(pubkey) == str(pubkey2):
-            return True
+
+    def verify_key (self, acctname, tokenkey):
+        if (re.match( r'^[A-Za-z0-9]+$', tokenkey) 
+                        and len(tokenkey) <= 64):
+            pubkey = PrivateKey(private_posting_key).pubkey or 0
+            pubkey2 = self.steem_instance().get_account(acctname)
+            if str(pubkey) == str(pubkey2['posting']['key_auths'][0][0]):
+                self.privatekey = tokenkey
+                self.refreshtoken = None
+                self.accesstoken = None
+                return True
+            else:
+                return False
         else:
-            return False
+            self.privatekey = None
+            self.accesstoken = self.connect.get_token(tokenkey)
+            if self.accesstoken:
+                if self.connect.username == acctname:
+                    self.refreshtoken = tokenkey
+                    return True
+                else:
+                    return False
+            else:
+                return False
 
 
 
     def reward_pool_balances(self):
-        reward_fund = self.steem_instance().get_reward_fund()
-        self.reward_balance = Amount(reward_fund["reward_balance"]).amount
-        self.recent_claims = float(reward_fund["recent_claims"])
-        self.base = Amount(self.steem_instance().get_current_median_history_price()["base"]).amount
+        try:
+            self.reward_balance
+        except:
+            reward_fund = self.steem_instance().get_reward_fund()
+            self.reward_balance = Amount(
+                reward_fund["reward_balance"]).amount
+            self.recent_claims = float(reward_fund["recent_claims"])
+            self.base = Amount(
+                self.steem_instance().get_current_median_history_price()["base"]
+                ).amount
         return [self.reward_balance, self.recent_claims, self.base]
 
 
 
     def rshares_to_steem (self, rshares):
-        try:
-            self.reward_balance
-        except:
-            self.reward_pool_balances()
-        return round(rshares * self.reward_balance / self.recent_claims * self.base, 4)
+        self.reward_pool_balances()
+        return round(
+            rshares * self.reward_balance / self.recent_claims * self.base,
+            4)
 
 
 
-    def current_vote_value(self, lastvotetime=None, steempower=0, voteweight=100, votepower=0):
+    def current_vote_value(self, lastvotetime=None, 
+            steempower=0, voteweight=100, votepower=0):
         c = Converter()
         voteweight = self.util.scale_vote(voteweight)
         if votepower > 0 and votepower < 101:
@@ -168,13 +178,15 @@ class SimpleSteem:
             vests = (float(vs) - float(dvests)) + float(rvests) 
             self.steempower = c.vests_to_sp(vests) or 0
             time.sleep(5)
-            return [self.sbdbal, self.steembal, self.steempower, self.votepower, self.lastvotetime]
+            return [self.sbdbal, self.steembal, self.steempower, 
+                    self.votepower, self.lastvotetime]
 
 
 
     def transfer_funds(self, to, amount, denom, msg):
         try:
-            self.steem_instance().commit.transfer(to, float(amount), denom, msg, self.mainaccount)
+            self.steem_instance().commit.transfer(to, 
+                float(amount), denom, msg, self.mainaccount)
         except Exception as e:
             self.msg.error_message(e)
             return False
@@ -187,7 +199,8 @@ class SimpleSteem:
         if not account:
             account = self.mainaccount
         try:
-            h = self.steem_instance().get_account_history(account, -1, limit)
+            h = self.steem_instance().get_account_history(
+                account, -1, limit)
         except Exception as e:
             self.msg.error_message(e)
             return False
@@ -201,9 +214,15 @@ class SimpleSteem:
     def post(self, title, body, permlink, tags):
         for num_of_retries in range(4):
             try:
-                self.steem_instance().post(title, body, self.mainaccount, permlink, None, None, None, None, tags, None, True)   
+                self.steem_instance().post(title, 
+                                            body, 
+                                            self.mainaccount, 
+                                            permlink, 
+                                            None, None, None, None, 
+                                            tags, None, True)   
             except Exception as e:
-                self.util.retry("COULD NOT POST '" + title + "'", e, num_of_retries, 10)
+                self.util.retry("COULD NOT POST '" + title + "'", 
+                    e, num_of_retries, 10)
             else:
                 time.sleep(60)
                 checkident = self.get_post_info(self.mainaccount)
@@ -211,16 +230,26 @@ class SimpleSteem:
                 if checkident == ident:
                     return True
                 else:
-                    self.util.retry("A POST JUST CREATED WAS NOT FOUND IN THE BLOCKCHAIN '" + title + "'", e, num_of_retries, 30)
+                    self.util.retry('''A POST JUST CREATED 
+                                    WAS NOT FOUND IN THE 
+                                    BLOCKCHAIN {}'''.format(title), 
+                                    e, num_of_retries, 30)
 
 
 
     def reply(self, permlink, msgbody):
         for num_of_retries in range(4): 
             try:
-                self.steem_instance().post("message", msgbody, self.mainaccount, None, permlink, None, None, "", None, None, False)
+                self.steem_instance().post("message", 
+                                            msgbody, 
+                                            self.mainaccount, 
+                                            None, 
+                                            permlink, 
+                                            None, None, "", 
+                                            None, None, False)
             except Exception as e:
-                self.util.retry("COULD NOT REPLY TO " + permlink, e, num_of_retries, 30)
+                self.util.retry("COULD NOT REPLY TO " + permlink, 
+                    e, num_of_retries, 30)
             else:
                 self.msg.message("Replied to " + permlink)
                 time.sleep(30)
@@ -229,7 +258,8 @@ class SimpleSteem:
 
     def follow(self, author):
         try:
-            self.steem_instance().commit.follow(author, ['blog'], self.mainaccount)
+            self.steem_instance().commit.follow(author, 
+                ['blog'], self.mainaccount)
         except Exception as e:
             self.msg.error_message(e)
             return False
@@ -240,7 +270,8 @@ class SimpleSteem:
 
     def unfollow(self, author):
         try:
-            self.steem_instance().commit.unfollow(author, ['blog'], self.mainaccount)
+            self.steem_instance().commit.unfollow(author, 
+                ['blog'], self.mainaccount)
         except Exception as e:
             self.msg.error_message(e)
             return False
@@ -254,7 +285,8 @@ class SimpleSteem:
             account = self.mainaccount
         following = []
         try:
-            temp = self.steem_instance().get_following(account, '', 'blog', 100)
+            temp = self.steem_instance().get_following(account, 
+                '', 'blog', 100)
         except Exception as e:
             self.msg.error_message(e)
             return False
@@ -272,12 +304,17 @@ class SimpleSteem:
             try:
                 blog = self.steem_instance().get_blog(author, 0, 30)
             except Exception as e:
-                self.util.retry("COULD NOT GET THE MOST RECENT POST FOR " + author, e, num_of_retries, 10)
+                self.util.retry('''COULD NOT GET THE 
+                                MOST RECENT POST FOR 
+                                {}'''.format(author), 
+                                e, num_of_retries, 10)
             else:
                 for p in blog:
                     age = self.util.days_back(p['comment']['created'])
                     if p['comment']['author'] == author and age == daysback:
-                        return self.util.identifier(p['comment']['author'], p['comment']['permlink'])
+                        return self.util.identifier(
+                            p['comment']['author'], 
+                            p['comment']['permlink'])
 
 
 
@@ -291,15 +328,19 @@ class SimpleSteem:
     def vote(self, identifier, weight):
         for num_of_retries in range(4):
             try:
-                self.steem_instance().vote(identifier, 100.0, self.mainaccount)
+                self.steem_instance().vote(identifier, 
+                    100.0, self.mainaccount)
                 self.msg.message("voted for " + identifier)
                 time.sleep(10)
             except Exception as e:
                 if re.search(r'You have already voted in a similar way', str(e)):
-                    self.msg.error_message("Already voted on " + identifier)
+                    self.msg.error_message('''Already voted on 
+                                        {}'''.format(identifier))
                     return "already voted"
                 else:
-                    self.util.retry("COULD NOT VOTE ON " + identifier, e, num_of_retries, 10)
+                    self.util.retry('''COULD NOT VOTE ON 
+                                    {}'''.format(identifier), 
+                                    e, num_of_retries, 10)
             else:
                 return True
 
@@ -308,11 +349,14 @@ class SimpleSteem:
     def resteem(self, identifier):
         for num_of_retries in range(4):
             try:
-                self.steem_instance().resteem(identifier, self.mainaccount)
+                self.steem_instance().resteem(
+                    identifier, self.mainaccount)
                 self.msg.message("resteemed " + identifier)
                 time.sleep(30)
             except Exception as e:
-                self.util.retry("COULD NOT RESTEEM " + identifier, e, num_of_retries, 10)
+                self.util.retry('''COULD NOT RESTEEM 
+                                {}'''.format(identifier), 
+                                e, num_of_retries, 10)
                 return False
             else:
                 return True
@@ -326,22 +370,15 @@ if __name__ == "__main__":
     s = SimpleSteem()
 
     followed = s.following()
-
-    num_of_followed = len(followed)
-
     pool = s.reward_pool_balances()
-
     bal = s.check_balances()
-
     vote_value = s.current_vote_value(bal[4], bal[2], 100, bal[3])
-
     votepower = (bal[3] + s.util.calc_regenerated(bal[4])) / 100
-
     identifier = s.recent_post()
 
     print ("\n        ____________" + s.mainaccount + "_____________\n")
     
-    print ("        Following " + str(num_of_followed) + " Steemians\n")
+    print ("        Following " + str(len(followed)) + " Steemians\n")
 
     print ("        Balances:\n")
 
--- simplesteem/steemconnectutil.py
+++ simplesteem/steemconnectutil.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 from steemconnect.client import Client
 from steemconnect.operations import Vote
 
@@ -10,63 +9,57 @@ class SteemConnect:
 
 
 
-    def __init__(self, client_id="", client_secret="", callback_url="", permissions="login,offline,vote"):
-
-        self.steemconnect = None
-
+    def __init__(self, client_id="", 
+                    client_secret="", 
+                    callback_url="", 
+                    permissions="login,offline,vote"):
         self.client_id = client_id
-
         self.client_secret = client_secret
-
         self.callback_url = callback_url
-
         self.permissions = permissions
+        self.sc = None
+        self.accesstoken = None
 
 
 
-    def connect(self, accesstoken=None): 
+    def steemconnect(self, accesstoken=None):
+        if self.sc:
+            return self.sc
         if accesstoken:
-            self.steemconnect = Client(access_token=accesstoken)
-
+            self.accesstoken = accesstoken
+        if self.accesstoken:
+            self.sc = Client(access_token=self.accesstoken)
         else:
-            self.steemconnect = Client(client_id=self.client_id, client_secret=self.client_secret)
+            self.sc = Client(client_id=self.client_id, 
+                            client_secret=self.client_secret)
+        return self.sc
 
 
 
     def get_token(self, code=None):
-
-        tokenobj = self.steemconnect.get_access_token(code)
-
+        tokenobj = self.steemconnect().get_access_token(code)
         for t in tokenobj:
-
             if t == 'error':
-
-                print (tokenobj[t])
-
                 return False
-
             elif t == 'access_token':
-
                 self.username = tokenobj['username']
-
                 self.refresh_token = tokenobj['refresh_token']
-
                 return tokenobj[t]
 
 
 
     def auth_url(self):
-
-        return self.steemconnect.get_login_url(self.callback_url, self.permissions, "get_refresh_token=True")
+        return self.steemconnect().get_login_url(
+                            self.callback_url, 
+                            self.permissions, 
+                            "get_refresh_token=True")
 
 
 
     def vote(self, voter, author, permlink, voteweight):
-
         vote = Vote(voter, author, permlink, voteweight)
-
-        result = self.steemconnect.broadcast([vote.to_operation_structure()])
-
+        result = self.steemconnect().broadcast(
+            [vote.to_operation_structure()])
         return result
 
 
@@ -76,7 +69,6 @@ class SteemConnect:
 if __name__ == "__main__":
 
     s = SteemConnect()
-    s.connect()
     print(s.auth_url())
 
 
