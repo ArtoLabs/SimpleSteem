5070d75fc0e6540463421f9fa00798b4ded2a5bb
ArtoLabs <artopium@gmail.com>
2018-07-11 07:32:15 -0600

    Added methods to calculate vests because steem.Converter gave errors

--- simplesteem/util.py
+++ simplesteem/util.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import urllib.request
 from urllib.error import HTTPError
 from screenlogger.screenlogger import Msg
+from steem.amount import Amount
 
 
 class Util:
@@ -14,6 +15,10 @@ class Util:
 
     def __init__(self, filename, path, screenmode):
         self.msg = Msg(filename, path, screenmode)
+        self.total_vesting_fund_steem = None
+        self.total_vesting_shares = None
+        self.vote_power_reserve_rate = None
+        self.info = None
 
 
     def goodnode(self, nodelist):
@@ -94,6 +99,40 @@ class Util:
         self.msg.error_message("Attempt number " + str(retry_num) + ". Retrying in " + str(waittime) + " seconds.")
         time.sleep(waittime)
 
+
+    def steem_per_mvests(self):
+        """ Obtain STEEM/MVESTS ratio
+        """
+        return (self.total_vesting_fund_steem /
+                (self.total_vesting_shares / 1e6))
+
+
+    def sp_to_vests(self, sp):
+        """ Obtain VESTS (not MVESTS!) from SP
+            :param number sp: SP to convert
+        """
+        return sp * 1e6 / self.steem_per_mvests()
+
+
+    def vests_to_sp(self, vests):
+        """ Obtain SP from VESTS (not MVESTS!)
+            :param number vests: Vests to convert to SP
+        """
+        return vests / 1e6 * self.steem_per_mvests()
+
+
+    def sp_to_rshares(self, sp, voting_power=10000, vote_pct=10000):
+        """ Obtain the r-shares
+            :param number sp: Steem Power
+            :param int voting_power: voting power (100% = 10000)
+            :param int vote_pct: voting participation (100% = 10000)
+        """
+        vesting_shares = int(self.sp_to_vests(sp) * 1e6)
+        used_power = int((voting_power * vote_pct) / 10000);
+        max_vote_denom = self.vote_power_reserve_rate * (5 * 60 * 60 * 24) / (60 * 60 * 24);
+        used_power = int((used_power + max_vote_denom - 1) / max_vote_denom)
+        rshares = ((vesting_shares * used_power) / 10000)
+        return rshares
 
 
 # EOF
